cbb73bd81d14b615cdf74d33581fe6839ca640ce
Alexander <volodarik9@gmail.com>
2021-10-11 00:24:23 +0300

    main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, Inject} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'front';
+ constructor( @Inject(DOCUMENT) private document: Document) {}
+ public  scrollChange =""
+
+  @HostListener("window:scroll", [])onWindowScroll() {
+    //.scrollTop
+    if(this.document.documentElement.scrollTop>90){
+       this.scrollChange ="black-important"
+    }else{
+      this.scrollChange ="";
+    }
+    //.clientHeight
+ //   console.log(this.document.documentElement.clientHeight);
+    //.innerHeight
+  //  console.log(window.innerHeight);
+  }
 }
